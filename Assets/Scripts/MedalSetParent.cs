b02dce3e4bf0b98f9c30a23c8fe6190cac48109c
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2023-12-09 18:52:13 +0900

    [add] メダルの親子関係操作スクリプト追加

--- Assets/Scripts/MedalSetParent.cs
+++ Assets/Scripts/MedalSetParent.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public class MedalSetParent : MonoBehaviour
 {
+    [SerializeField] bool _isUse = false;
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Medal"))
+        if (!_isUse)
+            return;
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            collision.transform.SetParent(transform);
+        }
+        else if (collision.gameObject.CompareTag("Medal"))
         {
             collision.transform.SetParent(transform);
         }
--- Assets/Scripts/MedalUnSetParent.cs
+++ Assets/Scripts/MedalUnSetParent.cs
@@ -2,9 +2,13 @@ using UnityEngine;
 
 public class MedalUnSetParent : MonoBehaviour
 {
+    [SerializeField] bool _isUse = false;
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Medal"))
+        if (!_isUse)
+            return;
+
+        if (collision.gameObject.CompareTag("Ground"))
         {
             collision.transform.SetParent(null);
         }
