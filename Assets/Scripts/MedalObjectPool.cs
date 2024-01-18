dca83eb3a4ea7a4d5da409845ba5c2fda0e6f81c
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-01-18 16:53:43 +0900

    [update] プールへ追加する際の生成位置をランダム化

--- Assets/Scripts/MedalObjectPool.cs
+++ Assets/Scripts/MedalObjectPool.cs
@@ -1,14 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Pool;
+using UnityEngine.Serialization;
 
 public class MedalObjectPool : Singleton<MedalObjectPool>
 {
-    [SerializeField] private int _defaultCapacity = 100;
-    [SerializeField] private GameObject[] _medalPrefabs;
-    private MedalSpawnerManager _medalSpawnerManager;
-    
+    [FormerlySerializedAs("_defaultCapacity")] [SerializeField] private int _defaultPoolCapacity = 100;
+    [SerializeField, Tooltip("生成するメダルの種類分プレハブを登録")] private GameObject[] _medalPrefabs;
+    [SerializeField, Tooltip("生成したメダルを格納したいゲームオブジェクトを登録")] private Transform[] _medalParent;
     private ObjectPool<GameObject> _pool;
+    private MedalSpawnerManager _medalSpawnerManager;
+
     public ObjectPool<GameObject> Pool => _pool;
 
     private void Start()
@@ -19,15 +21,16 @@ public class MedalObjectPool : Singleton<MedalObjectPool>
             actionOnRelease: target => target.SetActive(false), // オブジェクト解放処理
             actionOnDestroy: target => Destroy(target), // オブジェクト破棄時処理
             collectionCheck: true,
-            defaultCapacity: _defaultCapacity, // 最初のプールのサイズ
+            defaultCapacity: _defaultPoolCapacity, // 最初のプールのサイズ
             maxSize: 300
         );
     }
-
+    
     private GameObject CreatePooledItem()
     {
         var spawnerPos = GameObject.FindWithTag("Spawner").GetComponent<Transform>().position;
-        var random = Random.Range(0, 2);
-        return Instantiate(_medalPrefabs[random], spawnerPos, Quaternion.identity);
+        var randomSpawnPos = new Vector3(spawnerPos.x, spawnerPos.y, spawnerPos.z + Random.Range(-8, 8));
+        var randomMedal = Random.Range(0, 2); // TODO 召喚するメダルのランダムロジック作成
+        return Instantiate(_medalPrefabs[randomMedal], randomSpawnPos, Quaternion.identity, _medalParent[randomMedal]);
     }
 }
