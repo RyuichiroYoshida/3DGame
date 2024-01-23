3825dc17d7243092f6f1657b3b4ff2b4cf3a5d8b
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-01-24 00:59:31 +0900

    [add] Playerの入力でメダルを召喚するスクリプト追加

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 public class PlayerInputManager : MonoBehaviour
 {
     [SerializeField] private PauseController _pauseController;
+    [SerializeField] private SpawnMedal _spawnMedal;
     private void Update()
     {
         if (Input.GetButtonUp("Cancel"))
         {
             _pauseController.UsePauseWindow();
+        }
+        else if (Input.GetButtonDown("Jump"))
+        {
+            if (ScoreCounter.Instance.Score < StageManager.Instance.StageBetCount)
+            {
+                return;
+            }
+            ScoreCounter.Instance.SubScore(StageManager.Instance.StageBetCount);
+            _spawnMedal.MedalSpawn(MedalObjectPool.Instance.Pool);
         }
     }
 }
