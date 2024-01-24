ea4a644a09653f10d66f3cd1e1e0b57a4dee692d
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-01-24 20:33:56 +0900

    [add] フィーバー延長ロジック追加

--- Assets/Scripts/Spawner/FiverManager.cs
+++ Assets/Scripts/Spawner/FiverManager.cs
@@ -1,26 +1,44 @@
+using System;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class FiverManager : Singleton<FiverManager>
 {
-    [SerializeField, Header("フィーバー突入確率(%)")] private int _fiverChance = 10;
-    [SerializeField, Header("フィーバー突入確率 / this")] private int _fiverContinueChance = 3;
     [SerializeField] private int _fiverTime = 5;
     [SerializeField] private MedalSpawnerManager _medalSpawnerManager;
+    [SerializeField] private bool _isFiver = false;
+    [SerializeField, Header("フィーバー突入確率(%)")] private int _fiverChance = 10;
+    [SerializeField, Header("フィーバー継続確率 == フィーバー突入確率 / this")] private int _fiverContinueChance = 3;
 
+    public bool IsFiver => _isFiver;
     public void FiverCheck()
     {
+        if (_isFiver)
+        {
+            return;
+        }
         if (Random.Range(0, 101) <= _fiverChance)
         {
             FiverControllerAsync().Forget();
         }
     }
 
-    // TODO フィーバー継続抽選ロジック
+    private void FiverContinueCheck()
+    {
+        if (Random.Range(0, 101) <= _fiverContinueChance)
+        {
+            Debug.Log("ContinueFiver!");
+            FiverControllerAsync().Forget();
+        }
+    }
+
     private async UniTaskVoid FiverControllerAsync()
     {
-        _medalSpawnerManager.IsFiver = true;
-        await UniTask.Delay(_fiverTime);
-        _medalSpawnerManager.IsFiver = false;
+        Debug.Log("IsFiver!");
+        _isFiver = true;
+        await UniTask.Delay(TimeSpan.FromSeconds(_fiverTime));
+        _isFiver = false;
+        FiverContinueCheck();
     }
 }
