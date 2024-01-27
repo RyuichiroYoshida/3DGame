8db06563464d115d21f5a8202be83cbbad55c566
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-01-27 17:14:14 +0900

    [add] ゲージ消費時にスライダーをシェイクするスクリプト追加

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -8,6 +8,12 @@ public class BombController : Singleton<BombController>
     [SerializeField] private float _gaugeMaxValue = 100;
     [SerializeField] private float _tweenTime = 1;
     [SerializeField] private Explosion _explosion;
+    [Header("ゲージ消費時のシェイクパラメータ")]
+    [SerializeField] private float _shakeTime = 1;
+    [SerializeField] private float _shakePower = 2;
+    [SerializeField] private int _shakeCount = 30;
+    [SerializeField] private int _handShakeValue = 1;
+    
     private float _bombGauge = 0;
 
     private void Start()
@@ -15,12 +21,15 @@ public class BombController : Singleton<BombController>
         _gaugeSlider.maxValue = _gaugeMaxValue;
         _gaugeSlider.value = 0;
     }
+
     private void UseBombExplosion()
     {
         _explosion.Fire();
         _bombGauge = 0;
         _gaugeSlider.DOValue(0, _tweenTime);
+        _gaugeSlider.transform.DOShakePosition(_shakeTime, _shakePower, _shakeCount, _handShakeValue, false, true);
     }
+
     public void AddBombGauge(int addValue)
     {
         _bombGauge += addValue;
