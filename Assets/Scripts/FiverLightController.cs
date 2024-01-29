9600e30d75a770aa33128ccf4b31a59578c4f5f8
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-01-29 20:37:21 +0900

    [update] フィーバー時の光り方を調整

---
+++ Assets/Scripts/FiverLightController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class FiverLightController : MonoBehaviour
+{
+    [SerializeField] private float _speed = 1;
+    [Header("Dubug")]
+    [SerializeField] private float _h;
+    [SerializeField] private float _s;
+    [SerializeField] private float _v;
+    private Light _light;
+    private Color _defaultColor;
+    private void Start()
+    {
+        _light = GetComponent<Light>();
+        _defaultColor = _light.color;
+    }
+
+    private void Update()
+    {
+        if (FiverManager.Instance.IsFiver == false)
+        {
+            _light.color = _defaultColor;
+            return;
+        }
+        RainbowLight();
+    }
+
+    private void RainbowLight()
+    {
+        Color.RGBToHSV(_light.color, out _h, out _s, out _v);
+        if (_h >= 360)
+        {
+            _h = 0;
+        }
+        _s = 200;
+        _light.color = Color.HSVToRGB(_h + _speed * Time.deltaTime, _s, _v);
+    }
+}
