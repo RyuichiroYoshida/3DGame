30db97b1291d048fdb46d3fe602c45cf46c29827
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-01-30 23:58:36 +0900

    [update] 投射予測線の描画を調整

--- Assets/Scripts/Shooter/MedalShooter.cs
+++ Assets/Scripts/Shooter/MedalShooter.cs
@@ -4,9 +4,12 @@ public class MedalShooter : MonoBehaviour
 {
     [SerializeField] private Transform _middleTrans;
     [SerializeField] private Transform _endTrans;
-    [SerializeField] private Transform _markerTrans;
-    
     [SerializeField] private int _rendererMiddlePoint = 10;
+    [SerializeField] private GameObject _medalPrefab;
+    private Vector3 _startPos;
+    private Vector3 _middlePos;
+    private Vector3 _endPos;
+    
     private LineRenderer _lineRenderer;
 
     private void Start()
@@ -16,28 +19,21 @@ public class MedalShooter : MonoBehaviour
 
     private void Update()
     {
-        var startPos = transform.position;
-        var middlePos = _middleTrans.position;
-        var endPos = _endTrans.position;
+        _startPos = transform.position;
+        _middlePos = _middleTrans.position;
+        _endPos = _endTrans.position;
 
-        Debug.DrawLine(middlePos, endPos, Color.magenta);
-        if (Physics.Linecast(middlePos, endPos, out var hit))
-        {
-            endPos = hit.point;
-        }
-        
         var totalPoints = _rendererMiddlePoint + 2;
         _lineRenderer.positionCount = totalPoints;
-        _lineRenderer.SetPosition(0, startPos);
+        _lineRenderer.SetPosition(0, _startPos);
         for (var i = 1; i <= _rendererMiddlePoint; i++)
         {
             var t = i / ((float)totalPoints - 1);
-            var mPos = CreateCurve(startPos, middlePos, endPos, t);
+            var mPos = CreateCurve(_startPos, _middlePos, _endPos, t);
             _lineRenderer.SetPosition(i, mPos);
         }
 
-        _lineRenderer.SetPosition(totalPoints - 1, endPos);
-        _markerTrans.position = endPos;
+        _lineRenderer.SetPosition(totalPoints - 1, _endPos);
     }
 
     private Vector3 CreateCurve(Vector3 start, Vector3 middle, Vector3 end, float t)
@@ -46,5 +42,14 @@ public class MedalShooter : MonoBehaviour
         var q1 = Vector3.Lerp(middle, end, t);
         var q2 = Vector3.Lerp(q0, q1, t);
         return q2;
+    }
+
+    public void ShootMedal()
+    {
+        var obj = MedalObjectPool.Instance.Pool.Get();
+        obj.transform.position = transform.position;
+        obj.transform.rotation = Quaternion.Euler(90 + Random.Range(-20, 21), 0, 0);
+        obj.TryGetComponent(out Rigidbody rb);
+        rb.AddForce((_middlePos - _startPos) * 11, ForceMode.Impulse);
     }
 }
