e81889505b0bf7c16c745f46865e8d29c96a5cd2
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-01-30 23:57:51 +0900

    [add] メダルシューターの移動に制限を追加

--- Assets/Scripts/MedalShooterMove.cs
+++ Assets/Scripts/MedalShooterMove.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 public class MedalShooterMove : MonoBehaviour
 {
     [SerializeField] private float _speedHorizontal = 5;
-    [SerializeField] private float _speedVertical = 3;
     [SerializeField] private Rigidbody _rigidbody;
     
     public void MoveShooterHorizontal(float horizontal)
     {
-        var speed = _rigidbody.velocity;
-        speed = new Vector3(speed.x + (horizontal * _speedHorizontal), speed.y);
+        var pos = transform.position;
+        if (pos.z >= 8)
+        {
+            transform.position = new Vector3(pos.x, pos.y, 7.9f);
+        }
+        else if (pos.z <= -8)
+        {
+            transform.position = new Vector3(pos.x, pos.y, -7.9f);
+        }
+        else
+        {
+            var speed = _rigidbody.velocity;
+            _rigidbody.velocity = new Vector3(speed.x, speed.y, horizontal * _speedHorizontal);
+        }
     }
-    public void MoveShooterVertical(float vertical)
+    public void ResetVelocity()
     {
-        var speed = _rigidbody.velocity;
-        speed = new Vector3(speed.x, speed.y + (vertical * _speedVertical));
+        _rigidbody.velocity = Vector3.zero;
     }
 }
--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -3,33 +3,33 @@ using UnityEngine;
 public class PlayerInputManager : MonoBehaviour
 {
     [SerializeField] private PauseController _pauseController;
-    [SerializeField] private SpawnMedal _spawnMedal;
+    [SerializeField] private MedalShooter _medalShooter;
     [SerializeField] private MedalShooterMove _shooterMove;
     
     private void Update()
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
-        var vertical = Input.GetAxisRaw("Vertical");
         if (horizontal != 0)
         {
             _shooterMove.MoveShooterHorizontal(horizontal);
         }
-        else if (vertical != 0)
+        else
         {
-            _shooterMove.MoveShooterVertical(vertical);
+            _shooterMove.ResetVelocity();
         }
+        
         if (Input.GetButtonUp("Cancel"))
         {
             _pauseController.UsePauseWindow();
         }
-        else if (Input.GetButton("Jump"))
+        else if (Input.GetButtonDown("Jump"))
         {
             if (ScoreCounter.Instance.Score < StageManager.Instance.StageBetCount)
             {
                 return;
             }
             ScoreCounter.Instance.SubScore(StageManager.Instance.StageBetCount);
-            _spawnMedal.MedalSpawn(MedalObjectPool.Instance.Pool);
+            _medalShooter.ShootMedal();
         }
     }
 }
