4307c29f3330d420c59e4ed2629cd6a150f9bb03
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-02 01:39:17 +0900

    [add] ゲーム数カウント、獲得メダル枚数カウント機能追加

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -4,6 +4,19 @@ public class StageManager : Singleton<StageManager>
 {
     [SerializeField] private int _stageBetCount = 1;
     [SerializeField] private int _startCredit = 100;
+    
     public int StageBetCount => _stageBetCount;
     public int StartCredit => _startCredit;
+    public static int GameCount { get; private set; }
+    public static int MedalGetCount { get; private set; }
+
+    public void AddGameCount()
+    {
+        GameCount++;
+    }
+
+    public void AddMedalGetCount()
+    {
+        MedalGetCount++;
+    }
 }
