9c88b5b048aefd1700cb1563d91de9ad191afb2f
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-02 01:43:03 +0900

    [add] 抽選確率用構造体追加

---
+++ Assets/Scripts/Lottery/LotteryTable.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Lottery
+{
+    [System.Serializable]
+    public struct LotteryTable
+    {
+        [SerializeField, Tooltip("フィーバー直当たり確率")] private float _directFiver;
+        [SerializeField, Tooltip("フィーバーチャンス突入確率")] private float _fiverChallenge;
+        [SerializeField, Tooltip("フィーバーチャンス突破確率")] private float  _fiverChance;
+        [SerializeField, Tooltip("フィーバー継続確率")] private float _fiverContinue;
+        [SerializeField, Tooltip("小当たり確率")] private float _medalBonus;
+        
+        public float DirectFiver => _directFiver;
+        public float FiverChallenge => _fiverChallenge;
+        public float FiverChance => _fiverChance;
+        public float FiverContinue => _fiverContinue;
+        public float MedalBonus => _medalBonus;
+    }
+}
