95da85f20dc5503948ae36b740e2177397f3bbaf
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-02 22:41:39 +0900

    [add] 現在ステートを文字列で確認できる機能追加

--- Assets/Scripts/Lottery/ChanceManager.cs
+++ Assets/Scripts/Lottery/ChanceManager.cs
@@ -1,9 +1,11 @@
+using Lottery.State;
 using UnityEngine;
 
 namespace Lottery
 {
     public class ChanceManager : MonoBehaviour
     {
+        [SerializeField] private string _nowState;
         [SerializeField] private LotteryTable _lotteryTable;
         public float RandomValue { get; private set; }
         public StateMachine StateMachine { get; }
@@ -12,11 +14,17 @@ namespace Lottery
         private void Start()
         {
             StateMachine.Initialize(StateMachine.Normal);
+            StateMachine.StateChanged += StateView;
         }
 
         public void PlayGame()
         {
             RandomValue = Random.Range(1, 101);
+        }
+
+        private void StateView(IState state)
+        {
+            _nowState = state.GetType().Name;
         }
     }
 }
