fadb4f5fc5b870f12de98e30d6137619a43fda85
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-03 15:11:33 +0900

    [add] ステートマシンの初期化と更新処理追加、デバッグ用で現在のステートをテキストに表示する機能追加

--- Assets/Scripts/Lottery/ChanceManager.cs
+++ Assets/Scripts/Lottery/ChanceManager.cs
@@ -1,30 +1,44 @@
 using Lottery.State;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 namespace Lottery
 {
     public class ChanceManager : MonoBehaviour
     {
-        [SerializeField] private string _nowState;
         [SerializeField] private LotteryTable _lotteryTable;
-        public float RandomValue { get; private set; }
-        public StateMachine StateMachine { get; }
+        private StateMachine _stateMachine;
+        [Header("デバッグ用")]
+        [SerializeField] private Text _nowStateText;
+        [SerializeField] private float _randomValue;
+
+        public float RandomValue => _randomValue;
+        public StateMachine StateMachine => _stateMachine;
         public LotteryTable LotteryTable => _lotteryTable;
+
+        private void Awake()
+        {
+            _stateMachine = new StateMachine(this);
+            _lotteryTable = new LotteryTable(_lotteryTable);
+        }
 
         private void Start()
         {
-            StateMachine.Initialize(StateMachine.Normal);
-            StateMachine.StateChanged += StateView;
+            _stateMachine.Initialize(_stateMachine.Normal);
+            _stateMachine.StateChanged += StateView;
+            _stateMachine.StateUpdate();
         }
 
         public void PlayGame()
         {
-            RandomValue = Random.Range(1, 101);
+            _randomValue = Random.Range(1f, 101f);
+            _stateMachine.StateUpdate();
         }
 
         private void StateView(IState state)
         {
-            _nowState = state.GetType().Name;
+            _nowStateText.text = state?.GetType().Name;
         }
     }
 }
