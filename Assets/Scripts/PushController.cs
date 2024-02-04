86d58286d3c08d5726a7dd25e7667d90c98233d3
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-04 22:14:58 +0900

    [update] プッシャーの往復運動に遅延処理を追加、それに伴う移動処理を変更

--- Assets/Scripts/PushController.cs
+++ Assets/Scripts/PushController.cs
@@ -1,14 +1,33 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using DG.Tweening;
+
 public class PushController : MonoBehaviour
 {
     [SerializeField] private float _value = -3.5f;
     [SerializeField] private float _time = 1;
+    [SerializeField] private float _waitTime = 1f;
+
     private void Start()
     {
-        transform.DOMoveX(_value, _time)
-            .SetRelative(true)
-            .SetLink(this.gameObject)
-            .SetLoops(-1, LoopType.Yoyo);
+        var token = this.GetCancellationTokenOnDestroy();
+        MoveAsync(token).Forget();
+    }
+
+    private async UniTaskVoid MoveAsync(CancellationToken token)
+    {
+        while (true)
+        {
+            await UniTask.Delay(TimeSpan.FromSeconds(_waitTime), cancellationToken: token);
+            transform.DOMoveX(_value, _time / 2)
+                .SetRelative(true)
+                .SetLink(this.gameObject);
+            await UniTask.Delay(TimeSpan.FromSeconds(_waitTime), cancellationToken: token);
+            transform.DOMoveX(-_value, _time / 2)
+                .SetRelative(true)
+                .SetLink(this.gameObject);
+        }
     }
 }
