7bb711ae1a43e86086f5a958eab8bf21e79a0fe7
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-05 23:39:51 +0900

    [add] 確率変動時の遷移処理追加、乱数へのアクセス方法変更

--- Assets/Scripts/Lottery/State/Normal.cs
+++ Assets/Scripts/Lottery/State/Normal.cs
@@ -1,35 +1,48 @@
-using UnityEngine;
-
 namespace Lottery.State
 {
     public class Normal : IState
     {
         private ChanceManager _chanceManager;
+        private float _randomValue;
 
         public Normal(ChanceManager chanceManager)
         {
             _chanceManager = chanceManager;
         }
+
         public void Enter()
         {
-            
         }
 
         public void Update()
         {
-            if (_chanceManager.LotteryTable.DirectFiver >= _chanceManager.RandomValue)
-            {
-                _chanceManager.StateMachine.TransitionTo(_chanceManager.StateMachine.Fiver);
-            }
-            else if (_chanceManager.LotteryTable.FiverChallenge >= _chanceManager.RandomValue)
+            if (_chanceManager.IsChanceChangeMode || _chanceManager.IsMaxChanceMode)
             {
                 _chanceManager.StateMachine.TransitionTo(_chanceManager.StateMachine.FiverChance);
+                return;
+            }
+            _randomValue = _chanceManager.RandomValue;
+            if (_chanceManager.LotteryTable.DirectFiver >= _randomValue)
+            {
+                _chanceManager.StateMachine.TransitionTo(_chanceManager.StateMachine.Fiver);
+                return;
+            }
+
+            if (_chanceManager.LotteryTable.FiverChallenge >= _randomValue)
+            {
+                _chanceManager.StateMachine.TransitionTo(_chanceManager.StateMachine.FiverChance);
+                return;
+            }
+
+            if (_chanceManager.LotteryTable.MedalBonus >= _randomValue)
+            {
+                _chanceManager.StateMachine.TransitionTo(_chanceManager.StateMachine.Bonus);
             }
         }
 
         public void Exit()
         {
-            
+            _chanceManager.IsChanceChangeMode = false;
         }
     }
 }
