bad7c54481ce6bdaa359b62487dd5938d2d87ec9
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-05 23:38:15 +0900

    [update] 当選枚数管理構造体のTooltipをHeaderへ変更

--- Assets/Scripts/Lottery/LotteryMedal.cs
+++ Assets/Scripts/Lottery/LotteryMedal.cs
@@ -5,10 +5,12 @@ namespace Lottery
     [System.Serializable]
     public struct LotteryMedal
     {
-        [SerializeField, Tooltip("フィーバー時の獲得メダル")] private int _fiverMedalCount;
-        [SerializeField, Tooltip("小当たり時の獲得メダル")] private int _winMedalCount;
+        [SerializeField, Header("フィーバー時の獲得メダル")] private int _fiverMedalCount;
+        [SerializeField, Header("小当たり時の獲得メダル")] private int _winMedalCount;
 
+        /// <summary>フィーバー時の獲得メダル</summary>
         public int FiverMedalCount => _fiverMedalCount;
+        /// <summary>小当たり時の獲得メダル</summary>
         public int WinMedalCount => _winMedalCount;
         
         public LotteryMedal(LotteryMedal medal)
