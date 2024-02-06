8c357ac5913f697435491e83c89959688858232c
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-06 23:21:42 +0900

    [add] チャンス時の音、フィーバー状態のBGM、爆発音、小当たり音、フィーバー状態の落ちてくるメダル音　追加

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,7 +4,21 @@ public class AudioManager : Singleton<AudioManager>
 {
     [SerializeField] private AudioSource _medalAudioSource;
     [SerializeField] private AudioClip _medalDropClip;
-    
+    [SerializeField] private AudioSource _chanceAudio;
+    [SerializeField] private AudioSource _fiverBGM;
+    [SerializeField] private AudioSource _bombExplosion;
+    [SerializeField] private AudioSource _bonusAudio;
+    [SerializeField] private AudioSource _fiverDropMedal;
+
+    public AudioSource ChanceAudio => _chanceAudio;
+    public AudioSource FiverBGM => _fiverBGM;
+
+    public AudioSource BombExplosion => _bombExplosion;
+
+    public AudioSource BonusAudio => _bonusAudio;
+
+    public AudioSource FiverDropMedal => _fiverDropMedal;
+
     public void AMedalDrop()
     {
         _medalAudioSource.PlayOneShot(_medalDropClip);
