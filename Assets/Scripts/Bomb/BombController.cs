cd4de236c6911136b1de5ee239ba3fddfc7f1ed9
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-06 23:22:27 +0900

    [update] 爆発時にカメラも揺れるように変更、爆発音再生処理追加

--- Assets/Scripts/Bomb/BombController.cs
+++ Assets/Scripts/Bomb/BombController.cs
@@ -8,6 +8,7 @@ public class BombController : Singleton<BombController>
     [SerializeField] private float _gaugeMaxValue = 100;
     [SerializeField] private float _tweenTime = 1;
     [SerializeField] private Explosion _explosion;
+    [SerializeField] private Transform _cameraTransform;
     [Header("ゲージ消費時のシェイクパラメータ")]
     [SerializeField] private float _shakeTime = 1;
     [SerializeField] private float _shakePower = 2;
@@ -24,10 +25,12 @@ public class BombController : Singleton<BombController>
 
     private void UseBombExplosion()
     {
+        AudioManager.Instance.BombExplosion.Play();
         _explosion.Fire();
         _bombGauge = 0;
         _gaugeSlider.DOValue(0, _tweenTime);
-        _gaugeSlider.transform.DOShakePosition(_shakeTime, _shakePower, _shakeCount, _handShakeValue, false, true);
+        _gaugeSlider.transform.DOShakePosition(_shakeTime, _shakePower, _shakeCount, _handShakeValue);
+        _cameraTransform.DOShakePosition(_shakeTime, _shakePower / 2, _shakeCount, _handShakeValue);
     }
 
     public void AddBombGauge(int addValue)
