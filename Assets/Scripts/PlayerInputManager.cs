7ca5ba407db42d9880bf6c7bce1eb2943a94045b
RyuichiroYoshida <134368616+RyuichiroYoshida@users.noreply.github.com>
2024-02-06 23:26:44 +0900

    [update] 発射ボタンを長押し可能に変更、それに伴い発射にクールタイムが付くように変更

--- Assets/Scripts/PlayerInputManager.cs
+++ Assets/Scripts/PlayerInputManager.cs
@@ -5,9 +5,12 @@ public class PlayerInputManager : MonoBehaviour
     [SerializeField] private PauseController _pauseController;
     [SerializeField] private MedalShooter _medalShooter;
     [SerializeField] private MedalShooterMove _shooterMove;
-    
+    [SerializeField] private float _shootCoolTime;
+    private float _time;
+
     private void Update()
     {
+        _time += Time.deltaTime;
         var horizontal = Input.GetAxisRaw("Horizontal");
         if (horizontal != 0)
         {
@@ -17,17 +20,24 @@ public class PlayerInputManager : MonoBehaviour
         {
             _shooterMove.ResetVelocity();
         }
-        
+
         if (Input.GetButtonUp("Cancel"))
         {
             _pauseController.UsePauseWindow();
         }
-        else if (Input.GetButtonDown("Jump"))
+        else if (Input.GetButton("Jump"))
         {
+            if (_shootCoolTime >= _time)
+            {
+                return;
+            }
+
+            _time = 0;
             if (ScoreCounter.Score < StageManager.Instance.StageBetCount)
             {
                 return;
             }
+
             ScoreCounter.Instance.SubScore(StageManager.Instance.StageBetCount);
             _medalShooter.ShootMedal();
         }
